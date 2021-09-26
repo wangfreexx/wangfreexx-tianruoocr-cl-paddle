d17ce38a5e113d87f345432ba83e8a5288c7de35
aikebei <1034380608@qq.com>
2021-09-26 20:17:09 +0800

    刷新版本号

--- tianruoocr-master/Properties/AssemblyInfo.cs
+++ tianruoocr-master/Properties/AssemblyInfo.cs
@@ -6,17 +6,17 @@ using System.Runtime.InteropServices;
 using System.Security;
 using System.Security.Permissions;
 
-[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
 [assembly: AssemblyConfiguration("")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2021")]
 [assembly: AssemblyProduct("天若OCR文字识别本地版")]
 [assembly: AssemblyCompany("Github")]
 [assembly: Guid("b42ca6f6-3a1a-44b5-9993-eab600012397")]
-[assembly: AssemblyTitle("天若OCR文字识别本地版V1.0.1")]
+[assembly: AssemblyTitle("天若OCR文字识别本地版V1.0.2")]
 [assembly: AssemblyDescription("aikebeifree &Rochy_HE & 天若幽心")]
 [assembly: CompilationRelaxations(8)]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
 [assembly: AssemblyTrademark("")]
 [assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
 [assembly: Debuggable(DebuggableAttribute.DebuggingModes.Default | DebuggableAttribute.DebuggingModes.DisableOptimizations | DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints | DebuggableAttribute.DebuggingModes.EnableEditAndContinue)]
