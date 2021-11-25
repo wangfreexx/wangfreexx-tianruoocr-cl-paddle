6b059d189478fc4c3d25a178145b2d26f82a8e02
aikebei <1034380608@qq.com>
2021-11-25 16:25:52 +0800

    v1.2.6

--- tianruoocr-master/Properties/AssemblyInfo.cs
+++ tianruoocr-master/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 using System.Security;
 using System.Security.Permissions;
 
-[assembly: AssemblyVersion("1.2.5")]
+[assembly: AssemblyVersion("1.2.6")]
 [assembly: AssemblyConfiguration("")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2021")]
@@ -16,7 +16,7 @@ using System.Security.Permissions;
 [assembly: AssemblyTitle("天若OCR文字识别本地版V1.2.5")]
 [assembly: AssemblyDescription("aikebeifree &Rochy_HE & 天若幽心")]
 [assembly: CompilationRelaxations(8)]
-[assembly: AssemblyFileVersion("1.2.5")]
+[assembly: AssemblyFileVersion("1.2.6")]
 [assembly: AssemblyTrademark("")]
 [assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
 [assembly: Debuggable(DebuggableAttribute.DebuggingModes.Default | DebuggableAttribute.DebuggingModes.DisableOptimizations | DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints | DebuggableAttribute.DebuggingModes.EnableEditAndContinue)]
